3c5575cb440a44f17aa020b57e05a5cef885d79c
Sixeye <srinath@srinath.fr>
2020-01-29 23:23:17 +0100

    Main window

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,11 +1,12 @@
 from fbs_runtime.application_context.PySide2 import ApplicationContext
-from PySide2.QtWidgets import QMainWindow
 
 import sys
 
+from package.main_window import MainWindow
+
 if __name__ == '__main__':
     appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    window = QMainWindow()
+    window = MainWindow()
     window.resize(250, 150)
     window.show()
     exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
---
+++ src/main/python/package/main_window.py
@@ -0,0 +1,34 @@
+from PySide2 import QtWidgets
+
+class MainWindow(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setup_ui()
+
+    def setup_ui(self):
+        self.create_widget()
+        self.modify_widgets()
+        self.create_layouts()
+        self.add_widgets_to_layouts()
+        self.setup_connections()
+
+    def create_widget(self):
+        self.btn_clique = QtWidgets.QPushButton("Clique")
+
+    def modify_widgets(self):
+        pass
+
+    def create_layouts(self):
+        self.main_layout = QtWidgets.QVBoxLayout(self)
+
+    def add_widgets_to_layouts(self):
+        self.main_layout.addChildWidget(self.btn_clique)
+
+    def setup_connections(self):
+        self.btn_clique.clicked.connect(self.bouton_clicked)
+
+    def bouton_clicked(self):
+        message_box = QtWidgets.QMessageBox()
+        message_box.setWindowTitle("Bravo")
+        message_box.setText("C'est une première app!")
+        message_box.exec_()
